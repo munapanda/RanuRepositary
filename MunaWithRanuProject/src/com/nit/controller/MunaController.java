a9d9651603cbba0abffa0c547691ef2015e4dde3
dev1 <dev@gmail.com>
2020-09-29 12:07:30 +0530

    Second commit

--- MunaWithRanuProject/src/com/nit/controller/MunaController.java
+++ MunaWithRanuProject/src/com/nit/controller/MunaController.java
@@ -3,5 +3,6 @@ package com.nit.controller;
 public class MunaController {
  public static void main(String[] args) {
 	System.out.println("Muna have gooten");
+	System.out.println("Kuna good");
 }
 }
